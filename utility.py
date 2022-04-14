2ee029c8f96571cd18c2656af53d9bf8c6934e5a
Raja FAlih ghufran <75777945+Morphine19@users.noreply.github.com>
2022-04-14 16:38:28 +0700

    Add files via upload

---
+++ utility.py
@@ -0,0 +1,80 @@
+import  cv2     as cv 
+import pytesseract
+
+#convert image to grayscale 
+
+def grayscale(image) :
+    src = image
+
+    gray = cv.cvtColor(src,cv.COLOR_BGR2GRAY)
+
+    return gray
+
+
+def inverse_thresholding(image):
+    src =image
+    thresholdValue =  170 #93
+    maxVal = 255
+
+    ret,inversethresh = cv.threshold(src, thresholdValue, maxVal, cv.THRESH_BINARY_INV ) 
+    
+    return inversethresh
+
+
+
+def rotate(image):
+
+    src=image
+    
+    # grab the dimensions of the image and calculate the center of the image
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
+
+    # rotate our image by -90 degrees around the image
+    angle = 2
+    M = cv.getRotationMatrix2D((cX, cY), angle, 1.0)
+    rotated = cv.warpAffine(image, M, (w, h))
+
+    return rotated
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+#preprocessing with erosion
+
+
+# blur_image  = bilateral_filter(source)
+# grayscale_image = grayscale(blur_image)
+# erosion_image = erosion(grayscale_image)
+# canny_image = canny(erosion_image)
+
+
+
+# cv.imshow('1',blur_image)
+# cv.imshow('2',grayscale_image)
+# cv.imshow('3',erosion_image)
+# cv.imshow('4',canny_image)
+
+#preprocessing with thresholding
+
+# grayscale_image_2 = grayscale(source)
+# threshold_image = thresholding(grayscale_image_2)
+# canny_image_2 = canny(threshold_image)
+
+# cv.imshow ('1 thresh',grayscale_image_2)
+# cv.imshow('2 thresh',threshold_image)
+# cv.imshow('3 thresh',canny_image_2)
+
+#preprocessing with adaptive thresholding 
+
+# grayscale_image_3 = grayscale(source)
+# adaptive_threshold_image = adaptive_thresholding(grayscale_image_3)
+# canny_image_3  = canny(adaptive_threshold_image)
+
+# cv.imshow ('1 Adapt thresh',grayscale_image_3)
+# cv.imshow('2 Adapt thresh',adaptive_threshold_image)
+# cv.imshow('3 Adapt thresh',canny_image_3)
+
+
+cv.waitKey(0)
---
+++ videoinput.py
@@ -0,0 +1,29 @@
+# importing the necessary libraries
+import cv2 as cv
+import numpy as np
+from utility import *
+
+
+# Creating a VideoCapture object to read the video
+cap = cv.VideoCapture(0)
+
+
+# Loop until the end of the video
+while (cap.isOpened()):
+
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    #preprocessing goes here (edit frame variable)
+    
+
+    cv.imshow('frame', frame)
+    # define q as the exit button
+    if cv.waitKey(25) & 0xFF == ord('q'):
+        break
+
+# release the video capture object
+cap.release()
+# Closes all the windows currently opened.
+cv.destroyAllWindows()
+
