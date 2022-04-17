2162e5b0a3db0c45d8ddfa4ada15ac5b5878eb24
Raja FAlih ghufran <75777945+Morphine19@users.noreply.github.com>
2022-04-17 10:29:24 +0700

    Update influxtest.py

--- influxtest.py
+++ influxtest.py
@@ -4,9 +4,9 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate an API token from the "API Tokens Tab" in the UI
-token = "auz6q4v1Xg-O_vwIfLm0wIHUG-I3L4yhW7lqCtmUUepkrwTaoiKc3XP6dchap9WJpHXCIhiEAHUXuoYLoEmr9g=="
-org = "utm"
-bucket = "electricalmeter/autogen"
+token = "your token"
+org = "your org"
+bucket = "your bucket name"
 
 with InfluxDBClient(url="http://localhost:8086", token=token, org=org) as client:
 	
