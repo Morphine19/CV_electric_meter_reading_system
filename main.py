b3f138d86b7800623fc6700b2b9869341d456f33
Raja Falih ghufran <75777945+RajaFalih@users.noreply.github.com>
2022-05-04 17:47:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 # You can generate an API token from the "API Tokens Tab" in the UI
-token = "auz6q4v1Xg-O_vwIfLm0wIHUG-I3L4yhW7lqCtmUUepkrwTaoiKc3XP6dchap9WJpHXCIhiEAHUXuoYLoEmr9g=="
-org = "utm"
-bucket = "electricalmeter/autogen"
+token = "###"
+org = "###"
+bucket = "###"
 
 
 # Creating a VideoCapture object to read the video
